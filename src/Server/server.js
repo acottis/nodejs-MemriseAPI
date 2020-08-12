924d420889835052a2ff048330374e27eefcedd2
Adam Cottis <adam.cottis@outlook.com>
2020-08-12 17:07:57 +0100

    fix cookies

--- src/Server/server.js
+++ src/Server/server.js
@@ -75,6 +75,7 @@ app.post('/api/login', async (req, res) => {
 			res.cookie((name = 'id'), (val = result['id']), {
 				signed: true,
 				httpOnly: true,
+				sameSite: 'none',
 			});
 			res.json({
 				message: result['message'],
